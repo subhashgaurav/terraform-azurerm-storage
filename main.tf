90c26e7e793eee805ceb600162ad94646478939e
subhashgaurav <91779017+subhashgaurav@users.noreply.github.com>
2023-05-15 10:23:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resorce "azurerm_resource_group" "rg-1" {
+  name = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa-1" {
+  name = var.sa_name
+  resource_group_name = azurerm_resource_group.rg-1.name
+  location = azurerm_resource_group.rg-1.location
+  account_replication_type = "LRS"
+  account_tier = "Standard
+  
+  }"
+  
